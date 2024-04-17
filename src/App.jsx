ffe9b65b4bdb6a0b52905a29132e1bc2b31a4f4b
Jashan09 <navjashan.singh@cirvianum.cat>
2024-04-17 16:58:37 +0200

    funciona el llistar

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,79 @@
-import React from "react";
-import { useState } from "react";
-import Form from "./components/Form";
+import React, { useState } from 'react';
+import Form from './components/Form';
+import StudentList from './components/StudentList';
 
-function App() {
+const App = () => {
 
+  const [detallsEstudiant, setDetallsEstudiant] = useState([]);
+
+  const [tipusEstudiant, setTipusEstudiant] = useState('No Graduat');
   const [ngPlaces, setNGPlaces] = useState(60);
-const [gPlaces, setGPlaces] = useState(40);
+  const [gPlaces, setGPlaces] = useState(40);
 
-  const [tipusEstudiant, setTipusEstudiant] = useState("No Graduat");
   const handleChange = (e) => {
     setTipusEstudiant(e.target.value);
   };
-  // Afegim la funció que actualitza el valor de la variable d'estat places
+
   const setPlacesDisponibles = (updatedPlaces) => {
-    tipusEstudiant === "No Graduat"
-      ? setNGPlaces(updatedPlaces)
-      : setGPlaces(updatedPlaces);
+    tipusEstudiant === 'Graduat'
+      ? setGPlaces(updatedPlaces)
+      : setNGPlaces(updatedPlaces);
   };
-  
+
   return (
-    <div className="flex flex-col items-center justify-center gap-5 h-screen">
-      <div className="tipusEstudiant">
-
-{/* This is a comment */}
-
-        <label className="text-2xl text-center mx-2 block">
-          Places Disponibles No Graduats: {ngPlaces}
-          <br />
-          Places Disponibles Graduats: {gPlaces}
-        </label>
-        <label className="text-2xl mx-2">Selecciona Tipus del Estudiant:</label>
-        <select
-          className="appDropDown border rounded-md py-1 px-2"
-          onChange={handleChange}
-          value={tipusEstudiant}
-        >
-          <option value="No Graduat">No Graduat</option>
-          <option value="Graduat">Graduat</option>
-        </select>
+    <div className="App flex h-screen flex-col items-center justify-center ">
+      <div className="programes my-2">
+        <h3 className="title my-2 text-2xl text-blue-500">
+          Formulari d'inscripció d'estudiants.
+        </h3>
+        <ul className="ulInscripcio ">
+          <li
+            className="parentLabels my-2 flex items-center justify-evenly"
+            onChange={handleChange}
+          >
+            <label className="radioLabel">
+              <input
+                type="radio"
+                value="No Graduat"
+                name="programGroup"
+                defaultChecked
+                className="radioInput mr-2"
+              />
+              No Graduat
+            </label>
+            <label className="radioLabel">
+              <input
+                type="radio"
+                value="Graduat"
+                name="programGroup"
+                className="radioInput mr-2"
+              />
+              Graduat
+            </label>
+          </li>
+          <li className="parentLabels my-2">
+            Places disponibles per estudiant{' '}
+            <strong>
+              {tipusEstudiant}:{' '}
+              {tipusEstudiant === 'Graduat' ? gPlaces : ngPlaces}
+            </strong>
+          </li>
+        </ul>
       </div>
       <Form
   tipusEstudiantSelect={tipusEstudiant}
   setPlacesDisponibles={setPlacesDisponibles}
-  placesActuals={tipusEstudiant === "No Graduat" ? ngPlaces : gPlaces}
+  placesActuals={tipusEstudiant === 'PostGrau' ? gPlaces : ngPlaces}
+  setDetallsEstudiant={setDetallsEstudiant}
+/>
+
+<StudentList
+  detallsEstudiant={detallsEstudiant}
+  setDetallsEstudiant={setDetallsEstudiant}
 />
     </div>
+    
   );
-}
+};
 
 export default App;
